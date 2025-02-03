9f68ba398ac11d1b0aa79a5bc622b139049f992c
RaulToledoProcopio <rtolpro@gmail.com>
2025-02-03 10:32:47 +0100

    Noticia

--- src/main/kotlin/Repository/NoticiaRepository.kt
+++ src/main/kotlin/Repository/NoticiaRepository.kt
@@ -7,19 +7,24 @@ import org.example.Model.Noticia
 
 class NoticiaRepository(private val database: MongoDatabase) {
 
+    // Se obtiene la colección de noticias de la base de datos
     private val collection: MongoCollection<Document> = database.getCollection("ColeccionNoticias")
 
     fun publicarNoticia(noticia: Noticia) {
+        // Se crea un documento BSON con los datos de la noticia
         val noticiaDoc = Document("titulo", noticia.titulo)
             .append("cuerpo", noticia.cuerpo)
             .append("fecha_pub", noticia.fecha_pub)
             .append("autor", noticia.autor)
             .append("tags", noticia.tags)
-        collection.insertOne(noticiaDoc)
+
+        collection.insertOne(noticiaDoc) // Se inserta el documento en la colección de noticias
     }
 
     fun listarNoticiasPorUsuario(usuarioCorreo: String): List<Noticia> {
+        // Se filtran las noticias cuyo campo autor coincida con el correo proporcionado
         return collection.find(Document("autor", usuarioCorreo)).map { doc ->
+            // Se convierte cada documento en un objeto Noticia
             Noticia(
                 _id = doc.getObjectId("_id").toString(),
                 titulo = doc.getString("titulo"),
@@ -28,10 +33,11 @@ class NoticiaRepository(private val database: MongoDatabase) {
                 autor = doc.getString("autor"),
                 tags = doc.getList("tags", String::class.java)
             )
-        }.toList()
+        }.toList()  // Se convierte el resultado en una lista
     }
 
     fun buscarNoticiasPorEtiqueta(etiqueta: String): List<Noticia> {
+        // Se buscan noticias cuyo campo tags contenga la etiqueta proporcionada
         return collection.find(Document("tags", etiqueta)).map { doc ->
             Noticia(
                 _id = doc.getObjectId("_id").toString(),
@@ -45,6 +51,7 @@ class NoticiaRepository(private val database: MongoDatabase) {
     }
 
     fun listarUltimasNoticias(): List<Noticia> {
+        // Se ordenan las noticias por fecha de publicación en orden descendente y se limitan a 10
         return collection.find().sort(Document("fecha_pub", -1)).limit(10).map { doc ->
             Noticia(
                 _id = doc.getObjectId("_id").toString(),
